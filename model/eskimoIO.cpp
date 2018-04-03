8b7d733dc59665c9ddd50010d84f92a091c51f93
DaveAtKitronik <dave.sanderson@kitronik.co.uk>
2018-04-03 15:39:52 +0100

    Initial model
    created using circuit playground model as a base point

---
+++ model/eskimoIO.cpp
@@ -0,0 +1,75 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Lancaster University.
+Amended by Kitronik Ltd to support Eskimo range (c) 2018 Kitronik Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+/**
+  * Class definition for Eskimo IO
+  * Represents a collection of all I/O pins on the device.
+  */
+
+#include "CodalConfig.h"
+#include "EskimoIO.h"
+
+using namespace codal;
+
+/**
+  * Constructor.
+  *
+  * Create a representation of all given I/O pins on the edge connector
+  *
+  * Accepts a sequence of unique ID's used to distinguish events raised
+  * by MicroBitPin instances on the default EventModel.
+  */
+EskimoIO::EskimoIO() :
+    buttonA(ID_PIN_BUTTONA, PA04, PIN_CAPABILITY_DIGITAL),
+    buttonB(ID_PIN_BUTTONB, PA02, PIN_CAPABILITY_DIGITAL),
+    led (ID_PIN_LED, PA23, PIN_CAPABILITY_DIGITAL)
+{
+ /*
+ Copied here for safe keeping - as we add pins some of these should make their way into the initialise list above
+ a0 (ID_PIN_A0, PA02, PIN_CAPABILITY_AD),
+    a1 (ID_PIN_A1, PA05, PIN_CAPABILITY_AD),
+    a2 (ID_PIN_A2, PA06, PIN_CAPABILITY_AD),
+    a3 (ID_PIN_A3, PA07, PIN_CAPABILITY_AD),
+    a4 (ID_PIN_A4, PB03, PIN_CAPABILITY_AD),
+    a5 (ID_PIN_A5, PB02, PIN_CAPABILITY_AD),
+    a6 (ID_PIN_A6, PB09, PIN_CAPABILITY_AD),
+    a7 (ID_PIN_A7, PB08, PIN_CAPABILITY_AD),
+    speaker (ID_PIN_SPEAKER, PA02, PIN_CAPABILITY_AD),
+    microphoneData (ID_PIN_MIC_DATA, PA08, PIN_CAPABILITY_AD),
+    microphoneClock (ID_PIN_MIC_CLOCK, PA10, PIN_CAPABILITY_AD),
+
+    int1(ID_PIN_INT1, PA13, PIN_CAPABILITY_DIGITAL),
+    temperature(ID_PIN_THERMISTOR, PA09, PIN_CAPABILITY_AD),
+    light(ID_PIN_LIGHTSENSOR, PA11, PIN_CAPABILITY_AD),
+
+    buttonC(ID_PIN_BUTTONC, PA15, PIN_CAPABILITY_DIGITAL),
+    sda(ID_PIN_SDA, PA00, PIN_CAPABILITY_DIGITAL),
+    scl(ID_PIN_SCL, PA01, PIN_CAPABILITY_DIGITAL),
+    neopixel(ID_PIN_NEOPIXEL, PB23, PIN_CAPABILITY_DIGITAL),
+    flashMISO(ID_PIN_FLASH_MISO, PA16, PIN_CAPABILITY_DIGITAL),
+    flashMOSI(ID_PIN_FLASH_MOSI, PA20, PIN_CAPABILITY_DIGITAL),
+    flashSCLK(ID_PIN_FLASH_SCLK, PA21, PIN_CAPABILITY_DIGITAL),
+    flashSSEL(ID_PIN_FLASH_SSEL, PB22, PIN_CAPABILITY_DIGITAL)*/
+}
---
+++ model/eskimoIO.h
@@ -0,0 +1,120 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Lancaster University.
+Amended by Kitronik Ltd to support Eskimo range (c) 2018 Kitronik Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+
+*/
+
+#ifndef ESKIMO_IO_H
+#define ESKIMO_IO_H
+
+#include "mbed.h"
+#include "CodalConfig.h"
+#include "MbedPin.h"
+
+//
+// Component IDs for each pin.
+// The can be user defined, but uniquely identify a pin when using the eventing APIs/
+//
+#define ID_PIN_A0           (DEVICE_ID_IO_P0 + 0)
+#define ID_PIN_A1           (DEVICE_ID_IO_P0 + 1)
+#define ID_PIN_A2           (DEVICE_ID_IO_P0 + 2)
+#define ID_PIN_A3           (DEVICE_ID_IO_P0 + 3)
+#define ID_PIN_A4           (DEVICE_ID_IO_P0 + 4)
+#define ID_PIN_A5           (DEVICE_ID_IO_P0 + 5)
+#define ID_PIN_A6           (DEVICE_ID_IO_P0 + 6)
+#define ID_PIN_A7           (DEVICE_ID_IO_P0 + 7)
+#define ID_PIN_SCL          (ID_PIN_A4)
+#define ID_PIN_SDA          (ID_PIN_A5)
+#define ID_PIN_RX           (ID_PIN_A6)
+#define ID_PIN_TX           (ID_PIN_A7)
+#define ID_PIN_LED          (DEVICE_ID_IO_P0 + 19)
+#define ID_PIN_SPEAKER      (DEVICE_ID_IO_P0 + 20)
+#define ID_PIN_THERMISTOR   (DEVICE_ID_IO_P0 + 21)
+#define ID_PIN_LIGHTSENSOR  (DEVICE_ID_IO_P0 + 22)
+#define ID_PIN_INT1         (DEVICE_ID_IO_P0 + 23)
+#define ID_PIN_BUTTONA      (DEVICE_ID_IO_P0 + 24)
+#define ID_PIN_BUTTONB      (DEVICE_ID_IO_P0 + 25)
+#define ID_PIN_BUTTONC      (DEVICE_ID_IO_P0 + 26)
+#define ID_PIN_TOUCHDRIVE   (DEVICE_ID_IO_P0 + 27)
+#define ID_PIN_MIC_DATA     (DEVICE_ID_IO_P0 + 28)
+#define ID_PIN_MIC_CLOCK    (DEVICE_ID_IO_P0 + 29)
+#define ID_PIN_NEOPIXEL     (DEVICE_ID_IO_P0 + 30)
+#define ID_PIN_FLASH_MISO   (DEVICE_ID_IO_P0 + 31)
+#define ID_PIN_FLASH_MOSI   (DEVICE_ID_IO_P0 + 32)
+#define ID_PIN_FLASH_SCLK   (DEVICE_ID_IO_P0 + 33)
+#define ID_PIN_FLASH_SSEL   (DEVICE_ID_IO_P0 + 34)
+
+#define DEVICE_ID_BUTTON_C            1024
+
+namespace codal
+{
+    /**
+      * Represents a collection of all I/O pins exposed by the device.
+      */
+    class EskimoIO
+    {
+        public:
+/*
+        mbed::Pin          pin[0];
+        mbed::Pin          a0;
+        mbed::Pin          a1;
+        mbed::Pin          a2;
+        mbed::Pin          a3;
+        mbed::Pin          a4;
+        mbed::Pin          a5;
+        mbed::Pin          a6;
+        mbed::Pin          a7;
+        mbed::Pin          speaker;
+        mbed::Pin          microphoneData;
+        mbed::Pin          microphoneClock;
+        mbed::Pin          led;
+        mbed::Pin          int1;
+        mbed::Pin          temperature;
+        mbed::Pin          light;
+        mbed::Pin          buttonA;
+        mbed::Pin          buttonB;
+        mbed::Pin          buttonC;
+        mbed::Pin          sda;
+        mbed::Pin          scl;
+        mbed::Pin          neopixel;
+        mbed::Pin          flashMISO;
+        mbed::Pin          flashMOSI;
+        mbed::Pin          flashSCLK;
+        mbed::Pin          flashSSEL;
+*/
+
+        mbed::Pin          buttonA;
+        mbed::Pin          buttonB;
+        mbed::Pin          led;
+        /**
+          * Constructor.
+          *
+          * Create a representation of all given I/O pins
+          *
+          */
+        EskimoIO();
+    };
+}
+
+#endif
